f68753991259b6395d95f125723d110eecde1ed9
최이화 <ihwa220@naver.com>
2025-01-14 23:55:38 +0900

    refactor: BrowserRouter 위치 변경

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,16 +1,14 @@
 import TodoTemplate from "./components/TodoTemplate";
 import Posts from "./components/Posts";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<TodoTemplate />}/>
-        <Route path="/posts" element={<Posts />}></Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<TodoTemplate />}/>
+      <Route path="/posts" element={<Posts />}></Route>
+    </Routes>
   );
 }
 
